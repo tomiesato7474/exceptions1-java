1274e1cf802a3cf98e9786bd02a384afde60fcd9
Selma Tomie Sato <tomiesato74@gmail.com>
2024-02-26 08:37:27 -0300

    bad solution

--- src/application/Program9.java
+++ src/application/Program9.java
@@ -34,28 +34,14 @@ public class Program9 {
 				checkIn = sdf.parse(sc.next());
 				System.out.print("Check-out date (dd/MM/yyyy): ");
 				checkOut = sdf.parse(sc.next());
-				
-				Date now = new Date();
-				if (checkIn.before(now) || checkOut.before(now)) {
-					System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			
+  				String error = reservation.updateDates(checkIn, checkOut);
+				if (error != null) {
+					System.out.println("Error in reservation: " + error);
 				}
 				else {
-					if (!checkOut.after(checkIn)) {
-						System.out.println("Error in reservation: Check-out date must be after check-in date");
-					}
-					else {
-						reservation.updateDates(checkIn, checkOut);
-						System.out.println("Reservation: " + reservation);
-					}
+					System.out.println("Reservation: " + reservation);
 				}
-				
-//*				String error = reservation.updateDates(checkIn, checkOut);
-//*				if (error != null) {
-//*					System.out.println("Error in reservation: " + error);
-//*				}
-//*				else {
-//*					System.out.println("Reservation: " + reservation);
-//*				}
 			}
 			sc.close();
 		}
